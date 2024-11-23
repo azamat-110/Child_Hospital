c9260610d9aaaac460a0d7fcd90e8affeface5f7
azamat-110 <tursunbaevazamat565@gmail.com>
2024-11-24 00:04:31 +0500

    quasar config

---
+++ vue-quasar-frontend/src/components/EssentialLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+  >
+
+  </q-item>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'EssentialLink'
+})
+</script>
---
+++ vue-quasar-frontend/src/pages/UsersFetch.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, onMounted} from 'vue';
+
+const users = ref([]);
+
+const loadUsers = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/appoints');
+    if (!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`);
+    users.value = await response.json();
+    console.log(users.value);
+  } catch (error) {
+    console.error('Ошибка при загрузке данных:', error);
+
+  }
+};
+
+onMounted(() => {
+  loadUsers();
+});
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
