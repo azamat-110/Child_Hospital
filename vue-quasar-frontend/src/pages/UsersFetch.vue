b4b3744a727dd82df49e7e333649fc8ac05c92fa
azamat-110 <tursunbaevazamat565@gmail.com>
2024-11-24 12:54:34 +0500

    config

--- vue-quasar-frontend/src/pages/UsersFetch.vue
+++ vue-quasar-frontend/src/pages/UsersFetch.vue
@@ -3,25 +3,26 @@ import {ref, onMounted} from 'vue';
 
 const users = ref([]);
 
-const loadUsers = async () => {
-  try {
-    const response = await fetch('http://localhost:3001/api/appoints');
-    if (!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`);
-    users.value = await response.json();
-    console.log(users.value);
-  } catch (error) {
-    console.error('Ошибка при загрузке данных:', error);
-
-  }
-};
-
-onMounted(() => {
-  loadUsers();
-});
+// const loadUsers = async () => {
+//   try {
+//     const response = await fetch('http://localhost:3001/api/appoints');
+//     if (!response.ok) throw new Error(`Ошибка HTTP: ${response.status}`);
+//     users.value = await response.json();
+//     console.log(users.value);
+//   } catch (error) {
+//     console.error('Ошибка при загрузке данных:', error);
+//
+//   }
+// };
+//
+// onMounted(() => {
+//   loadUsers();
+// });
 </script>
 
 <template>
   <div>
+    {{users.length}}
 
   </div>
 </template>
